cc5fb9de1a9d84bf2d97eceefb496f3b26f1220e
Brandon Wulf <mrwulf@gmail.com>
2014-06-12 13:46:19 -0500

    A bit more logging

--- opt/service.bat
+++ opt/service.bat
@@ -26,16 +26,17 @@ for /f "usebackq skip=1 tokens=1,2*" %%a in (`reg query %var_puppet_key_name% /v
 set var_mcollective_base_dir=!var_programfilesx86_dir!\MCollective\
 set var_mcollective_etc_dir=%var_platform_program_data%\MCollective\etc\
 
+echo "Refreshing config" >> "%var_mcollective_etc_dir%startup.log" 2>&1
 echo ^
 main_collective = mcollective^
 
 collectives = mcollective^
 
-libdir = "!var_programfilesx86_dir!\MCollective\plugins"^
+libdir = !var_programfilesx86_dir!\MCollective\plugins^
 
 logfile = %var_platform_program_data%\MCollective\var\log\server.log^
 
-loglevel = info^
+loglevel = debug^
 
 daemonize = 1^
 
@@ -91,4 +92,4 @@ plugin.puppet.command = "!var_programfilesx86_dir!\Puppet Labs\Puppet\bin\puppet
 
 call "%var_puppet_base_dir%bin\environment.bat"
 
-"%var_puppet_base_dir%sys\ruby\bin\ruby.exe" -I"%var_mcollective_base_dir%lib;%var_puppet_base_dir%puppet\lib;%var_puppet_base_dir%facter\lib;%var_puppet_base_dir%hiera\lib;" -- "%var_mcollective_base_dir%bin\mcollectived" --config "%var_mcollective_etc_dir%server.cfg"
+"%var_puppet_base_dir%sys\ruby\bin\ruby.exe" -I"%var_mcollective_base_dir%lib;%var_puppet_base_dir%puppet\lib;%var_puppet_base_dir%facter\lib;%var_puppet_base_dir%hiera\lib;" -- "%var_mcollective_base_dir%bin\mcollectived" --config "%var_mcollective_etc_dir%server.cfg" >> "%var_mcollective_etc_dir%startup.log" 2>&1
